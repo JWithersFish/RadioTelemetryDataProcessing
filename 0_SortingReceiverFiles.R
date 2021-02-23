d809346b7854f15cc3175b196a1f1fa410cda97e
JLWithers <jonah_withers@fws.gov>
2021-02-23 10:10:58 -0500

    Updating folder names

--- 0_SortingReceiverFiles.R
+++ 0_SortingReceiverFiles.R
@@ -9,30 +9,30 @@
 
 # Moving hex files ----
 # Create directory
-if(!dir.exists(paste("../RawReceiverDownloads2020/ReceiverHexFiles"))){
-  dir.create(paste("../RawReceiverDownloads2020/ReceiverHexFiles"))
+if(!dir.exists(paste("../RawReceiverDownloads/ReceiverHexFiles"))){
+  dir.create(paste("../RawReceiverDownloads/ReceiverHexFiles"))
 } 
 
 # List files
-hxfiles <- list.files(path = "../RawReceiverDownloads2020",
+hxfiles <- list.files(path = "../RawReceiverDownloads",
                       pattern = "*.hex",
                       full.names = FALSE)
 
 # Move files
 for(i in 1:length(hxfiles)){
-  file.rename(from = paste("../RawReceiverDownloads2020/", hxfiles[i], sep = ""),
-            to = paste("../RawReceiverDownloads2020/ReceiverHexFiles/", hxfiles[i], sep = ""))
+  file.rename(from = paste("../RawReceiverDownloads/", hxfiles[i], sep = ""),
+            to = paste("../RawReceiverDownloads/ReceiverHexFiles/", hxfiles[i], sep = ""))
 }
 
 # List files
-hxfiles.1 <- list.files(path = "../RawReceiverDownloads2020/GMTPlus5",
+hxfiles.1 <- list.files(path = "../RawReceiverDownloads/GMTPlus5",
                       pattern = "*.hex",
                       full.names = FALSE)
 
 # Move files
 for(i in 1:length(hxfiles.1)){
-  file.rename(from = paste("../RawReceiverDownloads2020/GMTPlus5/", hxfiles.1[i], sep = ""),
-              to = paste("../RawReceiverDownloads2020/ReceiverHexFiles/", hxfiles.1[i], sep = ""))
+  file.rename(from = paste("../RawReceiverDownloads/GMTPlus5/", hxfiles.1[i], sep = ""),
+              to = paste("../RawReceiverDownloads/ReceiverHexFiles/", hxfiles.1[i], sep = ""))
 }
 
 
@@ -40,41 +40,41 @@ for(i in 1:length(hxfiles.1)){
 
 # Moving csv files ----
 # Create directory
-if(!dir.exists(paste("../RawReceiverDownloads2020/ReceiverCSVFiles"))){
-  dir.create(paste("../RawReceiverDownloads2020/ReceiverCSVFiles"))
+if(!dir.exists(paste("../RawReceiverDownloads/ReceiverCSVFiles"))){
+  dir.create(paste("../RawReceiverDownloads/ReceiverCSVFiles"))
 } 
 
 
 # List files
-csvfiles <- list.files(path = "../RawReceiverDownloads2020",
+csvfiles <- list.files(path = "../RawReceiverDownloads",
                       pattern = "*.csv",
                       full.names = FALSE)
 
 # Move files
 for(i in 1:length(csvfiles)){
-  file.rename(from = paste("../RawReceiverDownloads2020/", csvfiles[i], sep = ""),
-              to = paste("../RawReceiverDownloads2020/ReceiverCSVFiles/", csvfiles[i], sep = ""))
+  file.rename(from = paste("../RawReceiverDownloads/", csvfiles[i], sep = ""),
+              to = paste("../RawReceiverDownloads/ReceiverCSVFiles/", csvfiles[i], sep = ""))
 }
 
 # List files
-csvfiles.1 <- list.files(path = "../RawReceiverDownloads2020/GMTPlus5",
+csvfiles.1 <- list.files(path = "../RawReceiverDownloads/GMTPlus5",
                         pattern = "*.csv",
                         full.names = FALSE)
 
 # Move files
 for(i in 1:length(csvfiles.1)){
-  file.rename(from = paste("../RawReceiverDownloads2020/GMTPlus5/", csvfiles.1[i], sep = ""),
-              to = paste("../RawReceiverDownloads2020/ReceiverCSVFiles/", csvfiles.1[i], sep = ""))
+  file.rename(from = paste("../RawReceiverDownloads/GMTPlus5/", csvfiles.1[i], sep = ""),
+              to = paste("../RawReceiverDownloads/ReceiverCSVFiles/", csvfiles.1[i], sep = ""))
 }
 
 
 # Copy .txt files to training folders ----
 # First list files to get receiver site numbers
-txtfiles <- list.files(path = "../RawReceiverDownloads2020",
+txtfiles <- list.files(path = "../RawReceiverDownloads",
                          pattern = "*.txt",
                          full.names = FALSE)
 
-txtfiles.1 <- list.files(path = "../RawReceiverDownloads2020/GMTPlus5",
+txtfiles.1 <- list.files(path = "../RawReceiverDownloads/GMTPlus5",
                          pattern = "*.txt",
                          full.names = FALSE)
 
@@ -98,14 +98,14 @@ for(j in 1:length(Sites.0)){
 # Copy files to training directories ----
 for(h in 1:length(txtfiles.1)){
   FileSite <- substr(txtfiles.1[h], start = 0, stop = 3)
-  file.copy(from = paste("../RawReceiverDownloads2020/GMTPlus5/", txtfiles.1[h], sep = ""),
+  file.copy(from = paste("../RawReceiverDownloads/GMTPlus5/", txtfiles.1[h], sep = ""),
               to = paste("../1_CleaningWithAbtas/Python/Data/Training_Files_2020_", 
                          FileSite, sep = ""))
 }
 
 for(k in 1:length(txtfiles)){
   FileSite <- substr(txtfiles[k], start = 0, stop = 3)
-  file.copy(from = paste("../RawReceiverDownloads2020/", txtfiles[k], sep = ""),
+  file.copy(from = paste("../RawReceiverDownloads/", txtfiles[k], sep = ""),
             to = paste("../1_CleaningWithAbtas/Python/Data/Training_Files_2020_", 
                        FileSite, sep = ""))
 }
